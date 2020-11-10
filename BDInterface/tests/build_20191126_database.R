4d3a124737c801fcf52ad7a7ef90d0fd715b4961
nick-aemo <58719801+nick-aemo@users.noreply.github.com>
2020-11-10 15:44:58 +1100

    Update build_20191126_database.R

--- BDInterface/tests/build_20191126_database.R
+++ BDInterface/tests/build_20191126_database.R
@@ -1,17 +1,14 @@
-setwd(dirname(parent.frame(2)$ofile))
-source("DBInterface/R/interface.R")
+timeseries_path_name <- "data/2019-11-26/nsw_qld_20191126_data.csv"
+site_details_path_name <- "data/2019-11-26/site_details_nsw_qld.csv"
+circuit_details_path_name <- "data/2019-11-26/circuit_details_nsw_qld.csv"
 
-timeseries_path_name <- "../../data/2019-11-26/nsw_qld_20191126_data.csv"
-site_details_path_name <- "../../data/2019-11-26/site_details_nsw_qld.csv"
-circuit_details_path_name <- "../../data/2019-11-26/circuit_details_nsw_qld.csv"
-
-if (file.exists("20191126.db")){file.remove("20191126.db")}
+if (file.exists("data/2019-11-26/20191126_test_renv.db")){file.remove("data/2019-11-26/20191126_test_renv.db")}
 
 db <- DBInterface$new()
 db$default_timeseries_column_aliases <- list(utc_tstamp='_ts', c_id='_c_id', voltage='_v', 
                                              frequency='_f', energy='_e', duration='_d', power='_p')
-db$connect_to_new_database("20191126.db")
-db$add_postcode_lon_lat_to_database("testthat/data/postcode_lon_lat.csv")
+db$connect_to_new_database("data/2019-11-26/20191126_test_renv.db")
+db$add_postcode_lon_lat_to_database("postcode_lon_lat.csv")
 db$build_database(timeseries=timeseries_path_name,
                   circuit_details=circuit_details_path_name,
                   site_details=site_details_path_name)
