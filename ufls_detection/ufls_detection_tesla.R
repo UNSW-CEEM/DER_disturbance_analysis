75248826e6e19a7a7bbe6320c50ac16bd8abcd61
phoebeheywood <53634732+phoebeheywood@users.noreply.github.com>
2021-09-21 12:39:02 +1000

    Update ufls_detection_tesla.R
    Add option to fill nans or not, update time windows to include non-event bounds, set upper threshold to 180V

--- ufls_detection/ufls_detection_tesla.R
+++ ufls_detection/ufls_detection_tesla.R
@@ -1,9 +1,14 @@
 
-ufls_detection_voltage <- function(combined_data, event_time, window_length){
+ufls_detection_voltage <- function(combined_data, event_time, window_length, 
+                                   fill_nans=FALSE){
   pre_event_window_start <- event_time - 60 * window_length
   post_event_window_end <- event_time + 60 * window_length
-  pre_event_window <- filter(combined_data, ts > pre_event_window_start, ts < event_time)
-  post_event_window <- filter(combined_data, ts > event_time, ts < post_event_window_end)
+  pre_event_window <- filter(combined_data, 
+                             ts >= pre_event_window_start, 
+                             ts < event_time)
+  post_event_window <- filter(combined_data, 
+                              ts > event_time, 
+                              ts <= post_event_window_end)
 
   pre_event_voltage <- calc_average_voltage_per_circuit(pre_event_window)
   names(pre_event_voltage)[names(pre_event_voltage) == 'v_mean'] <- 'pre_event_v_mean'
@@ -13,13 +18,17 @@ ufls_detection_voltage <- function(combined_data, event_time, window_length){
 
   voltage_by_c_id <- merge(pre_event_voltage, post_event_voltage,
                            by=c('c_id', 'c_id'), all = TRUE)
-  voltage_by_c_id <- mutate(voltage_by_c_id, pre_event_v_mean = ifelse(is.na(pre_event_v_mean),
-                                                                      0, pre_event_v_mean))
-  voltage_by_c_id <- mutate(voltage_by_c_id, post_event_v_mean = ifelse(is.na(post_event_v_mean),
-                                                                       0, post_event_v_mean))
+  
+  if (fill_nans) {voltage_by_c_id <- mutate(voltage_by_c_id, 
+      post_event_v_mean = ifelse(is.na(post_event_v_mean),0,post_event_v_mean))}
+  
   ufls_dropout_voltage <- mutate(voltage_by_c_id,
-                         ufls_status_v = if_else((post_event_v_mean/pre_event_v_mean) < 0.2,
-                                               'UFLS Dropout', 'No UFLS Dropout'))
+      ufls_status_v = if_else((pre_event_v_mean > 180) &
+                              (post_event_v_mean/pre_event_v_mean < 0.5),
+                              'UFLS Dropout', 
+                              if_else(pre_event_v_mean > 180, 
+                                      'No UFLS Dropout', 
+                                      NA_character_)))
   return(ufls_dropout_voltage)
 }
 
