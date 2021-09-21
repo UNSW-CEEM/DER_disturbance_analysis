7d89a33158b59b618845c9d3a6cf22429c1944ae
phoebeheywood <53634732+phoebeheywood@users.noreply.github.com>
2021-09-21 16:12:38 +1000

    Create example_run_ufls_detection_voltage.R

---
+++ ufls_detection/example_run_ufls_detection_voltage.R
@@ -0,0 +1,13 @@
+source("load_tool_environment.R")
+
+db_file = "20210525_Tesla.db"
+
+ts <- sqldf::sqldf("select * from timeseries", dbname = db_file)
+
+ufls_statuses_v <- ufls_detection_voltage(combined_data = ts, 
+    event_time = strptime('2021-05-25 04:06:00', format="%Y-%m-%d %H:%M:%S", 
+                          tz="Australia/Brisbane"), 
+    window_length = 5, 
+    fill_nans = TRUE)
+
+write.csv(ufls_statuses_v, as.character("data/Tesla_ufls.csv"), row.names=FALSE)
