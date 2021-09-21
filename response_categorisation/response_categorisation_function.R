c98abf95f863ea418027622b66e08fea8af37cd0
michael-trollip-AEMO <michael.trollip@aemo.com.au>
2021-09-21 10:56:54 +1000

    Added NED threshold functionality
    Modified response categorisation algorithm, updated changes in shiny, added to manual settings.

--- response_categorisation/response_categorisation_function.R
+++ response_categorisation/response_categorisation_function.R
@@ -1,25 +1,34 @@
-categorise_response <- function(combined_data, event_time, window_length){
+# if first ts != event_time -> NA (also lets count how many lose to this)
+
+categorise_response <- function(combined_data, event_time, window_length,NED_threshold_pct = 0.8){
   event_window_data <- filter(combined_data, ts >= event_time & ts <= event_time + 60 * window_length)
   event_window_data <- event_window_data[order(event_window_data$ts),]
   event_window_data <- group_by(event_window_data, c_id, clean)
   event_window_data <- summarise(event_window_data, d=first(d), event_power=first(power_kW),
                                  min_norm_power=min(power_kW/first(power_kW)),
                                  num_con_zeros=num_consecutive_zeros(power_kW),
-                                 num_data_points=length(power_kW)-1)
-  event_window_data <- categorise_by_response(event_window_data, window_length)
+                                 num_data_points=length(power_kW)-1,
+                                 first_ts = ifelse(first(ts)==event_time,"sampled","not sampled"))
+  event_window_data <- categorise_by_response(event_window_data, window_length,NED_threshold_pct)
   event_window_data <- select(event_window_data, "c_id", "clean", "response_category")
   combined_data <- left_join(combined_data, event_window_data, on=c("c_id", "clean"))
   return(combined_data)
 }
 
-categorise_by_response <- function(event_window_data, window_length){
+categorise_by_response <- function(event_window_data, window_length,NED_threshold_pct){
   event_window_data <- mutate(event_window_data, response_category=ifelse(
     event_power < 0.1, '5 Off at t0', 'Undefined'))
   
+  # NED if sampled seconds below threshold
   event_window_data <- mutate(event_window_data, 
-                              response_category=ifelse(num_data_points < (60/d)*window_length, 
+                              response_category=ifelse(num_data_points < (60/d)*window_length*NED_threshold_pct,
+                                                       '6 Not enough data', response_category))
+  # NED if first_ts not sampled 
+  event_window_data <- mutate(event_window_data,
+                              response_category=ifelse(first_ts=="not sampled",
                                                        '6 Not enough data', response_category))
   
+  #
   event_window_data <- mutate(event_window_data, 
                               response_category=ifelse(response_category=='Undefined' & min_norm_power >= 0.96, 
                                                        '1 Ride Through', response_category))
@@ -32,6 +41,7 @@ categorise_by_response <- function(event_window_data, window_length){
   
   event_window_data <- mutate(event_window_data, response_category=ifelse(
     response_category=='Undefined' & num_con_zeros > 1, '4 Disconnect', response_category))
+  
   return(event_window_data)
 }
 
