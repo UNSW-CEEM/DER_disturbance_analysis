6a0176535769f564c838b179ce11a1f206547718
michael-trollip-AEMO <michael.trollip@aemo.com.au>
2021-09-29 09:46:16 +1000

    Update response_categorisation_function.R
    Added spaces between variables in the function arguments

--- response_categorisation/response_categorisation_function.R
+++ response_categorisation/response_categorisation_function.R
@@ -1,6 +1,6 @@
 # if first ts != event_time -> NA (also lets count how many lose to this)
 
-categorise_response <- function(combined_data, event_time, window_length,NED_threshold_pct = 0.8){
+categorise_response <- function(combined_data, event_time, window_length, NED_threshold_pct = 0.8){
   event_window_data <- filter(combined_data, ts >= event_time & ts <= event_time + 60 * window_length)
   event_window_data <- event_window_data[order(event_window_data$ts),]
   event_window_data <- group_by(event_window_data, c_id, clean)
@@ -9,13 +9,13 @@ categorise_response <- function(combined_data, event_time, window_length,NED_thr
                                  num_con_zeros=num_consecutive_zeros(power_kW),
                                  num_data_points=length(power_kW)-1,
                                  first_ts = ifelse(first(ts)==event_time,"sampled","not sampled"))
-  event_window_data <- categorise_by_response(event_window_data, window_length,NED_threshold_pct)
+  event_window_data <- categorise_by_response(event_window_data, window_length, NED_threshold_pct)
   event_window_data <- select(event_window_data, "c_id", "clean", "response_category")
   combined_data <- left_join(combined_data, event_window_data, on=c("c_id", "clean"))
   return(combined_data)
 }
 
-categorise_by_response <- function(event_window_data, window_length,NED_threshold_pct){
+categorise_by_response <- function(event_window_data, window_length, NED_threshold_pct){
   event_window_data <- mutate(event_window_data, response_category=ifelse(
     event_power < 0.1, '5 Off at t0', 'Undefined'))
   
@@ -33,7 +33,7 @@ categorise_by_response <- function(event_window_data, window_length,NED_threshol
                               response_category=ifelse(response_category=='Undefined' & min_norm_power >= 0.96, 
                                                        '1 Ride Through', response_category))
   
-  event_window_data <- mutate( event_window_data, response_category=ifelse(
+  event_window_data <- mutate(event_window_data, response_category=ifelse(
     response_category=='Undefined' & min_norm_power < 0.96 & num_con_zeros == 0, '2 Curtail', response_category))
   
   event_window_data <- mutate(event_window_data, response_category=ifelse(
