d084d48fb392a6b060b34def4905ec754c2b823a
phoebeheywood <53634732+phoebeheywood@users.noreply.github.com>
2021-11-25 18:11:31 +1100

    remove data table syntax

--- island_assessment/island_assessment_functions.R
+++ island_assessment/island_assessment_functions.R
@@ -15,25 +15,26 @@ classify_islands <- function(combined_data, alert_data, event_time, window_lengt
 # TODO: figure out which other functions rely on the responses/ need islanded sites removed
 
 identify_islanded_sites <- function(combined_data, alert_data, event_time){
-  if (~all(is.na(alert_data[,first_timestamp]))){
-    alert_data[, first_timestamp := as.POSIXct((first_timestamp)/1000, tz="Australia/Brisbane", origin="1970-01-01")]
-    alert_data[, Islanded := ifelse(is.na(first_timestamp),0,
+  if (!all(is.na(alert_data$first_timestamp))){
+    alert_data <- mutate(alert_data, first_timestamp = as.POSIXct((first_timestamp)/1000, tz="Australia/Brisbane", 
+                                                                  origin="1970-01-01"))
+    alert_data <- mutate(alert_data, Islanded = ifelse(is.na(first_timestamp),0,
                                     ifelse((GridFaultContactorTrip >= 1 | SYNC_a038_DoOpenArguments >= 1)
                                            & (first_timestamp < (event_time+30)),
-                                           1,0))]
+                                           1,0)))
   } else {
-    alert_data[, Islanded := ifelse((GridFaultContactorTrip >= 1 | SYNC_a038_DoOpenArguments >= 1), 1,0)]
+    alert_data <- mutate(alert_data, Islanded = ifelse((GridFaultContactorTrip >= 1 | SYNC_a038_DoOpenArguments >= 1), 1,0))
   }
 
-  combined_data <- merge(combined_data, alert_data[,c('c_id', 
+  combined_data <- left_join(combined_data, alert_data[,c('c_id', 
                                                   'Islanded',
                                                   'SYNC_a005_vfCheckUnderVoltage', 
                                                   'SYNC_a010_vfCheckFreqWobble')],
-                         by = c('c_id'), all.x = TRUE)
+                         by = c('c_id'))
   
-  lapply(c('SYNC_a005_vfCheckUnderVoltage', 'SYNC_a010_vfCheckFreqWobble'), function(x) {
-    combined_data[, paste0(x) := ifelse(is.na(combined_data[[x]]), 0, combined_data[[x]])]
-  })
+  #lapply(c('SYNC_a005_vfCheckUnderVoltage', 'SYNC_a010_vfCheckFreqWobble'), function(x) {
+  #  combined_data[, paste0(x) := ifelse(is.na(combined_data[[x]]), 0, combined_data[[x]])]
+  #})
 
   
   return(combined_data)
