97948b1a95f15b4d6b4b1bfa2b8fa5f08a750642
phoebeheywood <53634732+phoebeheywood@users.noreply.github.com>
2021-11-25 16:26:10 +1100

    Create island assessment

--- island_assessment/island_assessment_functions.R
+++ island_assessment/island_assessment_functions.R
@@ -21,11 +21,36 @@ classify_islands <- function(combined_data, alert_data, event_time){
                                                   'SYNC_a010_vfCheckFreqWobble')],
                          by = c('c_id'), all.x = TRUE)
   
-  # remove NAs
-  lapply(colnames(combined_data)[5:ncol(combined_data)], function(x) {
+  # fill NAs with 0
+  lapply(c('SYNC_a005_vfCheckUnderVoltage', 'SYNC_a010_vfCheckFreqWobble'), function(x) {
     combined_data[, paste0(x) := ifelse(is.na(combined_data[[x]]), 0, combined_data[[x]])]
   })
 
   
   return(combined_data)
+}
+
+assess_islands <- function(combined_data, event_time, window_length){
+  
+  event_window_data <- filter(combined_data, ts >= event_time & ts <= event_time + 60 * window_length)
+  event_window_data <- filter(event_window_data, Islanded & response_category %in% c('3 Drop to Zero', '4 Disconnect'))
+  
+  event_window_data <- groupby(event_window_data, c_id)
+  event_window_data <- summarise(event_window_data, 
+                                 max_f=max(frequency), min_f=min(frequency), 
+                                 max_v=(max(voltage) - 240) / 240, min_v=(240 - min(voltage)) / 240)
+  
+  event_window_data <- mutate(event_window_data, island_assessment=ifelse(max_f > 53, 'Gateway curtailed', 'Undefined'))
+  event_window_data <- mutate(event_window_data, 
+                              island_assessment=ifelse(island_assessment=='Undefined' & (min_f < 49.8 | max_f > 50.2), 
+                                                       'Frequency disruption', island_assessment))
+  event_window_data <- mutate(event_window_data, 
+                              island_assessment=ifelse(island_assessment=='Undefined' & (max_v > 0.1 | min_v > 0.1), 
+                                                       'Voltage disruption', island_assessment))
+  event_window_data <- mutate(event_window_data, 
+                              island_assessment=ifelse(island_assessment=='Undefined',
+                                                       'PV disconnect', island_assessment))
+
+  return(event_window_data)
+  
 }
