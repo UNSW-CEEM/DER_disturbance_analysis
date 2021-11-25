391c5b62417d00b0b8a17555e80f77350146f83f
phoebeheywood <53634732+phoebeheywood@users.noreply.github.com>
2021-11-25 16:49:51 +1100

    add main and replace responses with alerts

--- island_assessment/island_assessment_functions.R
+++ island_assessment/island_assessment_functions.R
@@ -1,10 +1,20 @@
-# TODO: add col to circ_sum with islanded flags, also for freqwobble and undervoltage. 
-# TODO: determine which islanded sites can be classified as disconnect
-# TODO: replace response_category with island_assessment response
+classify_islands <- function(combined_data, alert_data, event_time, window_length){
+  # add col to circ_sum with islanded flags, also for freqwobble and undervoltage. 
+  combined_data <- identify_islanded_sites(combined_data, alert_data, event_time)
+  # determine which islanded sites can be classified as disconnect
+  event_window_data <- filter(combined_data, ts >= event_time & ts <= event_time + 60 * window_length)
+  event_window_data <- assess_islands(event_window_data)
+  event_window_data <- select(event_window_data, "c_id", "clean", "island_assessment")
+  combined_data <- left_join(combined_data, event_window_data, on=c("c_id", "clean"))
+  # replace response_category with island_assessment response
+  combined_data <- replace_response_with_alert(combined_data)
+  return(combined_data)
+}
+
 # TODO: determine and apply hierarchy of responses
 # TODO: figure out which other functions rely on the responses/ need islanded sites removed
 
-classify_islands <- function(combined_data, alert_data, event_time){
+identify_islanded_sites <- function(combined_data, alert_data, event_time){
   if (~all(is.na(alert_data[,first_timestamp]))){
     alert_data[, first_timestamp := as.POSIXct((first_timestamp)/1000, tz="Australia/Brisbane", origin="1970-01-01")]
     alert_data[, Islanded := ifelse(is.na(first_timestamp),0,
@@ -21,7 +31,6 @@ classify_islands <- function(combined_data, alert_data, event_time){
                                                   'SYNC_a010_vfCheckFreqWobble')],
                          by = c('c_id'), all.x = TRUE)
   
-  # fill NAs with 0
   lapply(c('SYNC_a005_vfCheckUnderVoltage', 'SYNC_a010_vfCheckFreqWobble'), function(x) {
     combined_data[, paste0(x) := ifelse(is.na(combined_data[[x]]), 0, combined_data[[x]])]
   })
@@ -30,12 +39,9 @@ classify_islands <- function(combined_data, alert_data, event_time){
   return(combined_data)
 }
 
-assess_islands <- function(combined_data, event_time, window_length){
-  
-  event_window_data <- filter(combined_data, ts >= event_time & ts <= event_time + 60 * window_length)
+assess_islands <- function(event_window_data){
   event_window_data <- filter(event_window_data, Islanded & response_category %in% c('3 Drop to Zero', '4 Disconnect'))
-  
-  event_window_data <- groupby(event_window_data, c_id)
+  event_window_data <- groupby(event_window_data, c_id, clean)
   event_window_data <- summarise(event_window_data, 
                                  max_f=max(frequency), min_f=min(frequency), 
                                  max_v=(max(voltage) - 240) / 240, min_v=(240 - min(voltage)) / 240)
@@ -52,5 +58,14 @@ assess_islands <- function(combined_data, event_time, window_length){
                                                        'PV disconnect', island_assessment))
 
   return(event_window_data)
+}
+
+replace_response_with_alert <- function(combined_data){
+  combined_data <- mutate(combined_data, response_category=ifelse(Islanded, 'Islanded - Other', response_category))
+  combined_data <- mutate(combined_data, response_category=ifelse(SYNC_a005_vfCheckUnderVoltage>=1, 
+                                                                  'Islanded - Under Volt alert', response_category))
+  combined_data <- mutate(combined_data, response_category=ifelse(SYNC_a010_vfCheckFreqWobble>=1, 
+                                                                  'Islanded - Freq Wobble', response_category))
   
+  return(combined_data)
 }
