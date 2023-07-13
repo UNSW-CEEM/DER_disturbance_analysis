584ac01b0147c3cdf4e0a2d5e66ac9fd62188266
Christian Christiansen <christian.l.christiansen@gmail.com>
2023-07-13 11:48:30 +1000

    Extraneous space removal

--- reconnect_compliance/tests/test_categorise_reconnection_compliance.R
+++ reconnect_compliance/tests/test_categorise_reconnection_compliance.R
@@ -17,7 +17,7 @@ load_test_df <- function(text) {
 }
 
 testthat::test_that("Calculating categorisation works",{
-  
+
   reconnection_times <- "c_id,  response_category, ramp_above_threshold
                             1,       4-Disconnect,                 0.51
                             2,     3-Drop-to-Zero,                 0.50
@@ -25,21 +25,24 @@ testthat::test_that("Calculating categorisation works",{
                             4,     3-Drop-to-Zero,                 0.29
                             5,              other,                   NA
                             6,     3-Drop-to-Zero,                   NA"
-  
+
   reconnection_times <- load_test_df(reconnection_times)
-  
-  reconnection_times <- categorise_reconnection_compliance(reconnection_times,
-                                                           ramp_threshold_for_compliance = 0.3,
-                                                           ramp_threshold_for_non_compliance = 0.5)
-  
+
+  reconnection_times <- categorise_reconnection_compliance(
+    reconnection_times,
+    ramp_threshold_for_compliance = 0.3,
+    ramp_threshold_for_non_compliance = 0.5
+  )
+
   expected_categories <- "c_id, reconnection_compliance_status
-                             1,                  Non-Compliant 
+                             1,                  Non-Compliant
                              2,                         Unsure
                              3,                         Unsure
                              4,                      Compliant
                              5,                            NNN
                              6,                  Cannot-be-set"
-  
+
+
   expected_categories <- load_test_df_keep_NA(expected_categories)
-  testthat::expect_equal(reconnection_times, expected_categories, tolerance = 1e-4)
+  testthat::expect_equivalent(reconnection_times, expected_categories, tolerance = 1e-4)
 })
