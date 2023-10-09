8488ae4907c992e657e903aad373dfe918359822
Christian Christiansen <christian.l.christiansen@gmail.com>
2023-10-09 14:55:40 +0200

    Fix formatting

--- reconnect_compliance/tests/test_categorise_reconnection_compliance.R
+++ reconnect_compliance/tests/test_categorise_reconnection_compliance.R
@@ -2,22 +2,21 @@
 testthat::context("Testing categorising reconnections.")
 
 
-load_test_df_keep_NA <- function(text){
+load_test_df_keep_NA <- function(text) {
   text <- gsub(" ", "", text)
   text <- gsub("-", " ", text)
   df <- read.table(text = text, sep = ",", header = TRUE, stringsAsFactors = FALSE, na.strings = "NNN")
   return(df)
 }
 
-load_test_df <- function(text){
+load_test_df <- function(text) {
   text <- gsub(" ", "", text)
   text <- gsub("-", " ", text)
   df <- read.table(text = text, sep = ",", header = TRUE, stringsAsFactors = FALSE)
   return(df)
 }
 
-testthat::test_that("Calculating categorisation works",{
-  
+testthat::test_that("Calculating categorisation works", {
   reconnection_times <- "c_id,  response_category, ramp_above_threshold
                             1,       4-Disconnect,                 0.51
                             2,     3-Drop-to-Zero,                 0.50
@@ -25,21 +24,22 @@ testthat::test_that("Calculating categorisation works",{
                             4,     3-Drop-to-Zero,                 0.29
                             5,              other,                   NA
                             6,     3-Drop-to-Zero,                   NA"
-  
+
   reconnection_times <- load_test_df(reconnection_times)
-  
-  reconnection_times <- categorise_reconnection_compliance(reconnection_times,
-                                                           ramp_threshold_for_compliance = 0.3,
-                                                           ramp_threshold_for_non_compliance = 0.5)
-  
+  reconnection_times <- categorise_reconnection_compliance(
+    reconnection_times,
+    ramp_threshold_for_compliance = 0.3,
+    ramp_threshold_for_non_compliance = 0.5
+  )
+
   expected_categories <- "c_id, reconnection_compliance_status
-                             1,                  Non-Compliant 
+                             1,                  Non-Compliant
                              2,                         Unsure
                              3,                         Unsure
                              4,                      Compliant
                              5,                            NNN
                              6,                  Cannot-be-set"
-  
+
   expected_categories <- load_test_df_keep_NA(expected_categories)
   testthat::expect_equivalent(reconnection_times, expected_categories, tolerance = 1e-4)
 })
