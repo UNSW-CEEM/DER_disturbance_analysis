4a63578479d31520b7e06e0b342b862645151e0e
Christian Christiansen <christian.l.christiansen@gmail.com>
2023-10-09 15:11:33 +0200

    Fix formatting

--- reconnect_compliance/categorise_reconnection_compliance.R
+++ reconnect_compliance/categorise_reconnection_compliance.R
@@ -1,16 +1,14 @@
-
 catergorise_row <- function(response_category,
                             ramp_above_threshold,
                             ramp_threshold_for_compliance,
-                            ramp_threshold_for_non_compliance){
-  
-  if (response_category %in% c('4 Disconnect', '3 Drop to Zero')){
+                            ramp_threshold_for_non_compliance) {
+  if (response_category %in% c('4 Disconnect', '3 Drop to Zero')) {
     if (is.na(ramp_above_threshold)) {
       category <- 'Cannot be set'
     } else {
-      if(ramp_above_threshold < ramp_threshold_for_compliance){
+      if (ramp_above_threshold < ramp_threshold_for_compliance) {
         category <- 'Compliant'
-      } else if(ramp_above_threshold > ramp_threshold_for_non_compliance) {
+      } else if (ramp_above_threshold > ramp_threshold_for_non_compliance) {
         category <- 'Non Compliant'
       } else {
         category <- 'Unsure'
@@ -24,15 +22,17 @@ catergorise_row <- function(response_category,
 
 categorise_reconnection_compliance <- function(ramping_data,
                                                ramp_threshold_for_compliance,
-                                               ramp_threshold_for_non_compliance){
-  
-  ramping_data <- rowwise(ramping_data) %>% mutate(
-    reconnection_compliance_status = catergorise_row(response_category,
-                                                     ramp_above_threshold,
-                                                     ramp_threshold_for_compliance,
-                                                     ramp_threshold_for_non_compliance))
-  
+                                               ramp_threshold_for_non_compliance) {
+  ramping_data <- rowwise(ramping_data) %>%
+    mutate(
+      reconnection_compliance_status = catergorise_row(
+        response_category,
+        ramp_above_threshold,
+        ramp_threshold_for_compliance,
+        ramp_threshold_for_non_compliance
+      )
+    )
+
   ramping_data <- select(ramping_data, c_id, reconnection_compliance_status)
   return(ramping_data)
 }
-
