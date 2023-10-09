80cba92a7d9eb1a2f89630449a0ca6668dbc5f9f
Christian Christiansen <christian.l.christiansen@gmail.com>
2023-10-09 14:36:20 +0200

    Fix formatting

--- upscale_disconnections/summarise_disconnections.R
+++ upscale_disconnections/summarise_disconnections.R
@@ -1,4 +1,4 @@
-get_upscaling_results <- function(circuit_summary, manufacturer_install_data, event_date, region, sample_threshold){
+get_upscaling_results <- function(circuit_summary, manufacturer_install_data, event_date, region, sample_threshold) {
   # Upscale the proportion of disconnecting circuits by manufacturer to better represent the installed capacity.
   out <- list()
   disconnection_summary <- group_disconnections_by_manufacturer(circuit_summary)
@@ -13,8 +13,11 @@ get_upscaling_results <- function(circuit_summary, manufacturer_install_data, ev
   return(out)
 }
 
-get_upscaling_results_excluding_ufls_affected_circuits <- function(circuit_summary, manufacturer_install_data, event_date, region, 
-                                                 sample_threshold){
+get_upscaling_results_excluding_ufls_affected_circuits <- function(circuit_summary,
+                                                                   manufacturer_install_data,
+                                                                   event_date,
+                                                                   region,
+                                                                   sample_threshold) {
   # Upscale the proportion of disconnecting circuits based on sample sizes once UFLS circuits are removed.
   out <- list()
   disconnection_summary <- group_disconnections_by_manufacturer(circuit_summary, exclude_ufls_circuits = TRUE)
@@ -28,13 +31,13 @@ get_upscaling_results_excluding_ufls_affected_circuits <- function(circuit_summa
   disconnection_summary <- calc_confidence_intervals_for_disconnections(disconnection_summary)
   disconnection_summary <- calc_upscale_kw_loss(disconnection_summary)
   upscaled_disconnections <- upscale_disconnections(disconnection_summary)
-  disconnection_summary <- disconnection_summary %>% 
+  disconnection_summary <- disconnection_summary %>%
     rename(
       sample_size_after_removing_UFLS_affected_circuits = sample_size,
       cer_capacity_reduced_by_UFLS_proportion = cer_capacity
     )
   out$disconnection_summary <- disconnection_summary
-  upscaled_disconnections <- upscaled_disconnections %>% 
+  upscaled_disconnections <- upscaled_disconnections %>%
     rename(
       sample_size_after_removing_UFLS_affected_circuits = sample_size,
       cer_capacity_reduced_by_UFLS_proportion = cer_capacity
@@ -43,30 +46,31 @@ get_upscaling_results_excluding_ufls_affected_circuits <- function(circuit_summa
   return(out)
 }
 
-group_disconnections_by_manufacturer <- function(circuit_summary, exclude_ufls_circuits=FALSE){
+group_disconnections_by_manufacturer <- function(circuit_summary, exclude_ufls_circuits = FALSE) {
   # Don't count circuits without a well defined response type.
   if (exclude_ufls_circuits) {
+    # FIXME: Why is 6 used?
     bad_categories <- c("6 Not enough data", "Undefined", "NA", "UFLS Dropout")
   } else {
     bad_categories <- c("6 Not enough data", "Undefined", "NA")
   }
   circuit_summary <- filter(circuit_summary, !(response_category %in% bad_categories | is.na(response_category)))
-  
+
   # Get an initial summary of disconnection count and sample size by manufacturer.
   disconnection_summary <- group_by(circuit_summary, Standard_Version, manufacturer)
-  disconnection_summary <- summarise(disconnection_summary, 
+  disconnection_summary <- summarise(disconnection_summary,
                                      disconnections = get_number_of_disconnections(response_category),
                                      sample_size = length(response_category))
   return(disconnection_summary)
 }
 
-get_number_of_disconnections <- function(response_categories){
+get_number_of_disconnections <- function(response_categories) {
   disconnection_categories <- c("3 Drop to Zero", "4 Disconnect")
   response_categories <- response_categories[response_categories %in% disconnection_categories]
   return(length(response_categories))
 }
 
-get_number_of_ufls_disconnections <- function(response_categories){
+get_number_of_ufls_disconnections <- function(response_categories) {
   # Find the number of circuits identified as UFLS Dropout and the sample size to use for the UFLS proportion
   ufls_stats <- list()
   bad_categories <- c("6 Not enough data", "Undefined", "NA")
@@ -78,92 +82,114 @@ get_number_of_ufls_disconnections <- function(response_categories){
   return(ufls_stats)
 }
 
-scale_manufacturer_capacities_by_ufls <- function(disconnection_summary, ufls_stats){
+scale_manufacturer_capacities_by_ufls <- function(disconnection_summary, ufls_stats) {
   # Reduce the CER capacities by the UFLS proportion
   ufls_proportion <- ufls_stats$disconnections / ufls_stats$sample_size
-  disconnection_summary <- mutate(disconnection_summary, cer_capacity=cer_capacity*(1-ufls_proportion))
+  disconnection_summary <- mutate(disconnection_summary, cer_capacity = cer_capacity * (1 - ufls_proportion))
   # Add a UFLS row to the disconnection summary
-  ufls_row <- data.frame(Standard_Version="UFLS_disconnections_and_totals_including_ULFS_affected_circuits", 
-                         manufacturer="UFLS", disconnections=ufls_stats$disconnections, 
-                         sample_size=ufls_stats$sample_size, s_state="UFLS", 
-                         cer_capacity=sum(disconnection_summary$cer_capacity, na.rm = TRUE) / (1-ufls_proportion))
+  ufls_row <- data.frame(
+    Standard_Version = "UFLS_disconnections_and_totals_including_ULFS_affected_circuits",
+    manufacturer = "UFLS", disconnections = ufls_stats$disconnections,
+    sample_size = ufls_stats$sample_size, s_state = "UFLS",
+    cer_capacity = sum(disconnection_summary$cer_capacity, na.rm = TRUE) / (1-ufls_proportion)
+  )
   disconnection_summary <- rbind(disconnection_summary, ufls_row)
   return(disconnection_summary)
 }
 
-join_circuit_summary_and_cer_manufacturer_data <- function(circuit_summary, cer_manufacturer_data){
-  circuit_summary <- merge(circuit_summary, cer_manufacturer_data, by = c('Standard_Version', 'manufacturer'), 
-                           all = TRUE)
+join_circuit_summary_and_cer_manufacturer_data <- function(circuit_summary, cer_manufacturer_data) {
+  circuit_summary <- merge(
+    circuit_summary,
+    cer_manufacturer_data,
+    by = c("Standard_Version", "manufacturer"),
+    all = TRUE
+  )
   circuit_summary <- rename(circuit_summary, cer_capacity = capacity)
   return(circuit_summary)
 }
-  
-impose_sample_size_threshold <- function(disconnection_summary, sample_threshold){
-  circuit_summary <- mutate(disconnection_summary, sample_threshold, manufacturer = ifelse(is.na(cer_capacity), 
-                                                                                           'Other', manufacturer))
-  circuit_summary <- mutate(disconnection_summary, sample_threshold, manufacturer = ifelse(is.na(disconnections), 
-                                                                                           'Other', manufacturer))
-  
+
+impose_sample_size_threshold <- function(disconnection_summary, sample_threshold) {
+  circuit_summary <- mutate(
+    disconnection_summary,
+    sample_threshold,
+    manufacturer = ifelse(is.na(cer_capacity), "Other", manufacturer)
+  )
+  circuit_summary <- mutate(
+    disconnection_summary,
+    sample_threshold,
+    manufacturer = ifelse(is.na(disconnections), "Other", manufacturer)
+  )
+
   # Create an Other group for manufacturers with a small sample size.
   disconnection_summary <- mutate(disconnection_summary, sample_size = ifelse(is.na(sample_size), 0, sample_size))
-  disconnection_summary <- mutate(disconnection_summary, 
-                                  manufacturer = ifelse(sample_size < sample_threshold | 
-                                                          manufacturer == "Unknown" | 
-                                                          manufacturer == "Multiple" |
-                                                          manufacturer == "Mixed" | 
-                                                          manufacturer == "" |
-                                                          is.na(manufacturer), 
-                                                        "Other", manufacturer)
-                                  )
-  # Recalculate disconnection count and sample size. 
+  disconnection_summary <- mutate(
+    disconnection_summary,
+    # FIXME: Get rid of this nasty ifelse, or compartmentalise it.
+    manufacturer = ifelse(
+      (
+        sample_size < sample_threshold |
+        manufacturer == "Unknown" |
+        manufacturer == "Multiple" |
+        manufacturer == "Mixed" |
+        manufacturer == "" |
+        is.na(manufacturer)
+      ),
+      "Other",
+      manufacturer
+    )
+  )
+  # Recalculate disconnection count and sample size.
   disconnection_summary <- group_by(disconnection_summary, Standard_Version, manufacturer)
-  disconnection_summary <- summarise(disconnection_summary, 
-                                     disconnections = sum(disconnections, na.rm = TRUE),
-                                     sample_size = sum(sample_size, na.rm = TRUE),
-                                     cer_capacity = sum(cer_capacity, na.rm = TRUE))
-  
+  disconnection_summary <- summarise(
+    disconnection_summary,
+    disconnections = sum(disconnections, na.rm = TRUE),
+    sample_size = sum(sample_size, na.rm = TRUE),
+    cer_capacity = sum(cer_capacity, na.rm = TRUE)
+  )
+
   disconnection_summary <- mutate(disconnection_summary, proportion = disconnections / sample_size)
   disconnection_summary <- data.frame(disconnection_summary)
   return(disconnection_summary)
 }
 
-calc_confidence_intervals_for_disconnections <- function(disconnection_summary){
-  result <- mapply(CI_wrapper, disconnection_summary$disconnections, 
-                   disconnection_summary$sample_size, 0.95)
+calc_confidence_intervals_for_disconnections <- function(disconnection_summary) {
+  result <- mapply(CI_wrapper, disconnection_summary$disconnections, disconnection_summary$sample_size, 0.95)
   disconnection_summary$lower_bound <- result[1,]
   disconnection_summary$upper_bound <- result[2,]
   return(disconnection_summary)
 }
 
-CI_wrapper <- function(count, sample_size, ci){
+CI_wrapper <- function(count, sample_size, ci) {
   if (sample_size > 0) {
     result <- confidence_interval(count, sample_size, ci)
   } else {
     result <- c(NA, NA)
   }
-  
+
   return(result)
 }
 
-calc_upscale_kw_loss <- function(disconnection_summary){
-  disconnection_summary <- mutate(disconnection_summary, 
-                                  predicted_kw_loss = proportion * cer_capacity,
-                                  lower_bound_kw_loss = lower_bound * cer_capacity,
-                                  upper_bound_kw_loss = upper_bound * cer_capacity)
+calc_upscale_kw_loss <- function(disconnection_summary) {
+  disconnection_summary <- mutate(
+    disconnection_summary,
+    predicted_kw_loss = proportion * cer_capacity,
+    lower_bound_kw_loss = lower_bound * cer_capacity,
+    upper_bound_kw_loss = upper_bound * cer_capacity
+  )
   return(disconnection_summary)
 }
 
-get_manufactures_in_input_db_but_not_cer <- function(disconnection_summary){
+get_manufactures_in_input_db_but_not_cer <- function(disconnection_summary) {
   disconnection_summary <- filter(disconnection_summary, is.na(cer_capacity))
   return(disconnection_summary)
 }
 
-get_manufactures_in_cer_but_not_input_db <- function(disconnection_summary){
+get_manufactures_in_cer_but_not_input_db <- function(disconnection_summary) {
   disconnection_summary <- filter(disconnection_summary, is.na(sample_size))
   return(disconnection_summary)
 }
 
-get_manufacturer_capacitys <- function(manufacturer_install_data, event_date, region){
+get_manufacturer_capacitys <- function(manufacturer_install_data, event_date, region) {
   manufacturer_install_data <- filter(manufacturer_install_data, s_state == region)
   manufacturer_install_data <- manufacturer_install_data[order(manufacturer_install_data$date), ]
   manufacturer_install_data <- filter(manufacturer_install_data, date <= event_date)
@@ -173,20 +199,19 @@ get_manufacturer_capacitys <- function(manufacturer_install_data, event_date, re
   return(manufacturer_install_data)
 }
 
-upscale_disconnections <- function(manufacturer_level_summary){
+upscale_disconnections <- function(manufacturer_level_summary) {
   upscaled_disconnections <- group_by(manufacturer_level_summary, Standard_Version)
-  upscaled_disconnections <- summarise(upscaled_disconnections,
-                                       sample_size = sum(sample_size),
-                                       unscaled_disconnections = sum(disconnections) / sum(sample_size),
-                                       cer_capacity = sum(cer_capacity),
-                                       lower_bound_kw_loss = sum(lower_bound_kw_loss),
-                                       predicted_kw_loss = sum(predicted_kw_loss),
-                                       upper_bound_kw_loss = sum(upper_bound_kw_loss),                         
-                                       lower_bound = sum(lower_bound_kw_loss)/sum(cer_capacity),
-                                       disconnections = sum(predicted_kw_loss)/sum(cer_capacity),
-                                       uppper_bound = sum(upper_bound_kw_loss)/sum(cer_capacity))
+  upscaled_disconnections <- summarise(
+    upscaled_disconnections,
+    sample_size = sum(sample_size),
+    unscaled_disconnections = sum(disconnections) / sum(sample_size),
+    cer_capacity = sum(cer_capacity),
+    lower_bound_kw_loss = sum(lower_bound_kw_loss),
+    predicted_kw_loss = sum(predicted_kw_loss),
+    upper_bound_kw_loss = sum(upper_bound_kw_loss),
+    lower_bound = sum(lower_bound_kw_loss) / sum(cer_capacity),
+    disconnections = sum(predicted_kw_loss) / sum(cer_capacity),
+    uppper_bound = sum(upper_bound_kw_loss) / sum(cer_capacity)
+  )
   return(upscaled_disconnections)
 }
-
-
-
--- upscale_disconnections/tests/test_summarise_disconnections.R
+++ upscale_disconnections/tests/test_summarise_disconnections.R
@@ -1,16 +1,15 @@
-
 testthat::context("Testing summarising disconnections.")
 
 
-load_test_file <- function(text){
+load_test_file <- function(text) {
   text <- gsub(" ", "", text)
   text <- gsub("-", " ", text)
   df <- read.table(text = text, sep = ",", header = TRUE, stringsAsFactors = FALSE)
   return(df)
 }
 
-testthat::test_that("group_disconnections_by_manufacturer",{
-  
+testthat::test_that("group_disconnections_by_manufacturer", {
+
   circuit_summary <- "c_id, Standard_Version, manufacturer, response_category
                          1,                y,          SMA, 6-Not-enough-data
                          2,                y,          SMA,         Undefined
@@ -22,11 +21,11 @@ testthat::test_that("group_disconnections_by_manufacturer",{
                          8,                z,            x,              blah
                          9,                z,            x,              blah
                         10,                z,            x,              blah"
-  
+
   expected_output <- "Standard_Version, manufacturer, disconnections, sample_size
                                      y,          SMA,              2,           3
                                      y,            x,              1,           1
-                                     z,            x,              0,           3" 
+                                     z,            x,              0,           3"
 
   circuit_summary <- load_test_file(circuit_summary)
   expected_output <- load_test_file(expected_output)
@@ -36,23 +35,23 @@ testthat::test_that("group_disconnections_by_manufacturer",{
 
 testthat::test_that("join_circuit_summary_and_cer_manufacturer_data",{
 
-  
+
   disconnection_summary <- "Standard_Version, manufacturer, disconnections, sample_size
                                            y,          SMA,              2,           3
                                            y,            x,              1,           1
                                            z,            x,              0,           3"
-  
+
   cer_data <- "Standard_Version, manufacturer, capacity
                               y,          SMA,       10
                               y,            a,       11
                               z,            x,       12"
-  
+
   expected_output <- "Standard_Version, manufacturer, disconnections, sample_size, cer_capacity
                                      y,            a,             NA,          NA,          11
                                      y,          SMA,              2,           3,          10
                                      y,            x,              1,           1,          NA
                                      z,            x,              0,           3,          12"
-  
+
   disconnection_summary <- load_test_file(disconnection_summary)
   cer_data <- load_test_file(cer_data)
   expected_output <- load_test_file(expected_output)
@@ -61,8 +60,8 @@ testthat::test_that("join_circuit_summary_and_cer_manufacturer_data",{
 })
 
 testthat::test_that("impose_sample_size_threshold",{
-  
-  
+
+
   input <- "Standard_Version, manufacturer, disconnections, sample_size, cer_capacity
                            y,        Other,             NA,          NA,           11
                            y,          SMA,              2,           3,           10
@@ -70,12 +69,12 @@ testthat::test_that("impose_sample_size_threshold",{
                            y,       Unkown,              1,           1,           NA
                            y,        Other,              1,           1,           NA
                            z,            x,              0,          30,           12"
-  
-  
+
+
   expected_output <- "Standard_Version, manufacturer, disconnections, sample_size, cer_capacity, proportion
                                      y,        Other,              5,           6,           21,  0.8333333
                                      z,            x,              0,          30,           12,          0"
-  
+
   input <- load_test_file(input)
   expected_output <- load_test_file(expected_output)
   output <- impose_sample_size_threshold(input, sample_threshold = 30)
@@ -84,20 +83,20 @@ testthat::test_that("impose_sample_size_threshold",{
 })
 
 testthat::test_that("calc_confidence_intervals_for_disconnections",{
-  
-  
+
+
   input <- "Standard_Version, manufacturer, disconnections, sample_size, cer_capacity, proportion
                            y,        Other,              5,           6,           21,  0.8333333
                            z,            x,              0,          30,           12,          0"
-  
+
   expected_output <- "Standard_Version, manufacturer, disconnections, sample_size, cer_capacity, proportion, lower_bound, upper_bound
                                      y,        Other,              5,           6,           21,  0.8333333,   0.3587654,   0.9957893
                                      z,            x,              0,          30,           12,          0,         0.0,   0.1157033"
-  
+
   # Clopper-Pearson confidence interval for n = 6, 5 succcesses is [0.3587654, 0.9957893]
   # Clopper-Pearson confidence interval for  n = 30, 0 successes is [0, 0.1157033]
   # Source: https://epitools.ausvet.com.au/ciproportion
-  
+
   input <- load_test_file(input)
   expected_output <- load_test_file(expected_output)
   output <- calc_confidence_intervals_for_disconnections(input)
@@ -105,15 +104,15 @@ testthat::test_that("calc_confidence_intervals_for_disconnections",{
 })
 
 testthat::test_that("calc_upscale_kw_loss",{
-  
+
   input <- "Standard_Version, manufacturer, disconnections, sample_size, cer_capacity, proportion, lower_bound, upper_bound
-                           y,        Other,              5,           6,           21,  0.8333333,   0.3587654,   0.9957893  
+                           y,        Other,              5,           6,           21,  0.8333333,   0.3587654,   0.9957893
                            z,            x,              0,          30,           12,          0,         0.0,   0.1157033"
-  
+
   expected_output <- "Standard_Version, manufacturer, disconnections, sample_size, cer_capacity, proportion, lower_bound, upper_bound, predicted_kw_loss, lower_bound_kw_loss, upper_bound_kw_loss
                                      y,        Other,              5,           6,           21,  0.8333333,   0.3587654,   0.9957893,              17.5,           7.5340734,          20.9115753
-                                     z,            x,              0,          30,           12,          0,         0.0,   0.1157033,               0.0,                 0.0,           1.3884396"            
-  
+                                     z,            x,              0,          30,           12,          0,         0.0,   0.1157033,               0.0,                 0.0,           1.3884396"
+
   input <- load_test_file(input)
   expected_output <- load_test_file(expected_output)
   output <- calc_upscale_kw_loss(input)
