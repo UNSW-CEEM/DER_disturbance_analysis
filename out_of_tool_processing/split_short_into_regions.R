a5f17b718d051e4647af680cccc654b5d4518b7c
Christian Christiansen <christian.l.christiansen@gmail.com>
2023-10-17 11:26:32 +0200

    Fix formatting, and for loop

--- out_of_tool_processing/split_short_into_regions.R
+++ out_of_tool_processing/split_short_into_regions.R
@@ -1,10 +1,6 @@
 ###INPUT CSV FILES
 #Rename your csv file into the format: "short_YYYY-MM-DD.csv"
-# FIXME: Remove hard-coded filenames
 temp.csv <- "short_2019-03-03.csv"
-
-##Set Working Directory to the root file.
-
 
 ## Script to split
 df.c_id <- read.csv("circuit_details.csv", header = TRUE, stringsAsFactors = FALSE) %>%
@@ -14,7 +10,7 @@ df.site_id <- read.csv("site_details.csv", header = TRUE, stringsAsFactors = FAL
   select(site_id, s_state)
 df.site_id <- distinct(df.site_id)
 
-df.circuit_state <- left_join(df.c_id, df.site_id, by="site_id") %>%
+df.circuit_state <- left_join(df.c_id, df.site_id, by = "site_id") %>%
   select(c_id, s_state) %>%
   mutate(c_id = as.character(c_id))
 
@@ -23,52 +19,49 @@ df.circuit_state <- distinct(df.circuit_state)
 row_count_file <- NULL
 row_count_region_file <- NULL
 
-# FIXME: i is not a good variable name.
-for (i in temp.csv) {
-  temp.df <- read.csv(i, header = TRUE, stringsAsFactors = FALSE)
+temp.df <- read.csv(temp_csv, header = TRUE, stringsAsFactors = FALSE)
 
-  df.ts_state <- left_join(temp.df, df.circuit_state, by="c_id")
+df.ts_state <- left_join(temp.df, df.circuit_state, by = "c_id")
 
-  all_regions <- unique(df.ts_state$s_state)
+all_regions <- unique(df.ts_state$s_state)
 
-  temp.row_count <- as.data.frame(nrow(temp.df)) %>%
-    mutate(new_df_count= nrow(df.ts_state)) %>%
-    mutate(File = paste(i))
+temp.row_count <- as.data.frame(nrow(temp.df)) %>%
+  mutate(new_df_count = nrow(df.ts_state)) %>%
+  mutate(File = paste(temp_csv))
 
-  names(temp.row_count) <- c("OriginalRowCount","NewRowCount", "File")
+names(temp.row_count) <- c("OriginalRowCount", "NewRowCount", "File")
 
-  row_count_file <- rbind(row_count_file, temp.row_count)
+row_count_file <- rbind(row_count_file, temp.row_count)
 
-  # FIXME: ii is not a good variable name.
-  for (ii in all_regions) {
-    if (is.na(ii) == TRUE) {
-      temp.NA.df <- filter(df.ts_state, is.na(s_state))
+for (region in all_regions) {
+  if (is.na(region) == TRUE) {
+    temp.NA.df <- filter(df.ts_state, is.na(s_state))
 
-      na.c_id <- as.data.frame(unique(temp.NA.df$c_id))
-      names(na.c_id) <- "UnmatchedCiDs"
+    na.c_id <- as.data.frame(unique(temp.NA.df$c_id))
+    names(na.c_id) <- "UnmatchedCiDs"
 
-      write.csv(na.c_id, paste(substr(i, 7, 16), "_", ii, "short.csv"))
+    write.csv(na.c_id, paste(substr(temp_csv, 7, 16), "_", region, "short.csv"))
 
-      temp.row_count_region <- as.data.frame(nrow(temp.NA.df)) %>%
-        mutate(File = paste(i)) %>%
-        mutate(Region= paste(ii))
+    temp.row_count_region <- as.data.frame(nrow(temp.NA.df)) %>%
+      mutate(File = paste(temp_csv)) %>%
+      mutate(Region = paste(region))
 
-      names(temp.row_count_region) <- c("RowCount", "File", "Region")
-    } else {
-      temp.region.df <- filter(df.ts_state, s_state==ii) %>%
-        select(c_id, utc_tstamp, energy, power, voltage, frequency, duration)
+    names(temp.row_count_region) <- c("RowCount", "File", "Region")
+  } else {
+    temp.region.df <- filter(df.ts_state, s_state == region) %>%
+      select(c_id, utc_tstamp, energy, power, voltage, frequency, duration)
 
-      write.csv(temp.region.df, paste(substr(i, 7, 16), "_", ii, "short.csv"))
+    write.csv(temp.region.df, paste(substr(temp_csv, 7, 16), "_", region, "short.csv"))
 
-      temp.row_count_region <- as.data.frame(nrow(temp.region.df)) %>%
-        mutate(File = paste(i)) %>%
-        mutate(Region= paste(ii))
+    temp.row_count_region <- as.data.frame(nrow(temp.region.df)) %>%
+      mutate(File = paste(temp_csv)) %>%
+      mutate(Region = paste(region))
 
-      names(temp.row_count_region) <- c("RowCount", "File", "Region")
-    }
-    row_count_region_file <- rbind(row_count_region_file, temp.row_count_region)
+    names(temp.row_count_region) <- c("RowCount", "File", "Region")
   }
+  row_count_region_file <- rbind(row_count_region_file, temp.row_count_region)
 }
+
 
 write.csv(row_count_file, "Original_total_rows.csv")
 
