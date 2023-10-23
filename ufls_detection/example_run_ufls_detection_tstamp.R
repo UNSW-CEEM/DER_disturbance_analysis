6c7cb722cc6abd2a22f04665910b7aafe08757a1
Christian Christiansen <christian.l.christiansen@gmail.com>
2023-10-23 16:25:30 +0200

    Fix formatting

--- ufls_detection/example_run_ufls_detection_tstamp.R
+++ ufls_detection/example_run_ufls_detection_tstamp.R
@@ -6,11 +6,11 @@ db$connect_to_existing_database("Data/2021-05-25 QLD/20210525_QLD_database_new.d
 
 ufls_statuses_ts <- ufls_detection_tstamp(
   db = db,
-  region = 'NSW',
-  pre_event_interval = strptime('2021-05-25 14:06:40', format="%Y-%m-%d %H:%M:%S", tz="Australia/Brisbane"),
+  region = "NSW",
+  pre_event_interval = strptime("2021-05-25 14:06:40", format = "%Y-%m-%d %H:%M:%S", tz = "Australia/Brisbane"),
   pre_event_window_length = 5,
   post_event_window_length = 9,
   pre_pct_sample_seconds_threshold = 0.6
 )
 
-write.csv(ufls_statuses_ts, as.character("Data/2021-05-25 QLD/1406/NSW_new_criteria_UFLS.csv"), row.names=FALSE)
+write.csv(ufls_statuses_ts, as.character("Data/2021-05-25 QLD/1406/NSW_new_criteria_UFLS.csv"), row.names = FALSE)
