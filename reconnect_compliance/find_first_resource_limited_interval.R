edc8e367f1fbce73c2ef42f7f9e46c44abf76d39
Christian Christiansen <christian.l.christiansen@gmail.com>
2023-10-23 16:15:10 +0200

    Fix formatting

--- reconnect_compliance/find_first_resource_limited_interval.R
+++ reconnect_compliance/find_first_resource_limited_interval.R
@@ -4,7 +4,7 @@ find_first_resource_limited_interval <- function(ramp_rates, ramp_rate_change_th
     mutate(ramp_rate_change = (ramp_rate - lag(ramp_rate))) %>%
     filter(ramp_rate_change < ramp_rate_change_threshold) %>%
     group_by(c_id) %>%
-    summarise(resource_limited_interval = first(ts))
+    summarise(resource_limited_interval = first(ts)) %>%
     select(c_id, resource_limited_interval)
   ramp_rates <- as.data.frame(ramp_rates)
   return(ramp_rates)
