ebb29f24f06a0386a7c620bb5752be1cac0327be
Christian Christiansen <christian.l.christiansen@gmail.com>
2023-10-23 16:33:32 +0200

    Fix formatting

--- upscaling/upscale_function.R
+++ upscaling/upscale_function.R
@@ -6,7 +6,7 @@ upscale <- function(performance_data, install_capacity) {
   performance_data <- mutate(performance_data, date = as.Date(ts))
   install_capacity <- install_capacity[order(install_capacity$date), ]
   install_capacity <- filter(install_capacity, date < min(performance_data$date))
-  install_capacity <- group_by(install_capacity, Standard_Version, Grouping,  s_state)
+  install_capacity <- group_by(install_capacity, Standard_Version, Grouping, s_state)
   install_capacity <- summarise(install_capacity, standard_capacity = last(standard_capacity))
   install_capacity <- as.data.frame(install_capacity)
   performance_and_install <- inner_join(performance_data, install_capacity, by = c("Standard_Version", "s_state"))
@@ -28,7 +28,7 @@ calc_site_performance_factors <- function(performance_data) {
   performace_data_p <- group_by(performance_data, ts, site_id, clean)
   performace_data_p <- summarise(performace_data_p, site_performance_factor = sum(power_kW))
   performace_data_p <- as.data.frame(performace_data_p)
-  performance_data <- left_join(performance_data, performace_data_p, by = c('site_id', 'ts', 'clean'))
-  performance_data <- mutate(performance_data, site_performance_factor = (site_performance_factor/sum_ac))
+  performance_data <- left_join(performance_data, performace_data_p, by = c("site_id", "ts", "clean"))
+  performance_data <- mutate(performance_data, site_performance_factor = (site_performance_factor / sum_ac))
   return(performance_data)
 }
