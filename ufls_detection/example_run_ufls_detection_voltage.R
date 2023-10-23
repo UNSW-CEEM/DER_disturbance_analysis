4be5341f1109ee9a984e7e894fe2f2bc8ba4f6d6
Christian Christiansen <christian.l.christiansen@gmail.com>
2023-10-23 16:27:30 +0200

    Fix formatting

--- ufls_detection/example_run_ufls_detection_voltage.R
+++ ufls_detection/example_run_ufls_detection_voltage.R
@@ -4,10 +4,11 @@ db_file <- "20210525_Tesla.db"
 
 ts <- sqldf::sqldf("select * from timeseries", dbname = db_file)
 
-ufls_statuses_v <- ufls_detection_voltage(combined_data = ts, 
-    event_time = strptime('2021-05-25 04:06:00', format="%Y-%m-%d %H:%M:%S", 
-                          tz="Australia/Brisbane"), 
-    window_length = 5, 
-    fill_nans = TRUE)
+ufls_statuses_v <- ufls_detection_voltage(
+  combined_data = ts,
+  event_time = strptime("2021-05-25 04:06:00", format = "%Y-%m-%d %H:%M:%S", tz = "Australia/Brisbane"),
+  window_length = 5,
+  fill_nans = TRUE
+)
 
-write.csv(ufls_statuses_v, as.character("data/Tesla_ufls.csv"), row.names=FALSE)
+write.csv(ufls_statuses_v, as.character("data/Tesla_ufls.csv"), row.names = FALSE)
