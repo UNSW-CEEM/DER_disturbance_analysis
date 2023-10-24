10d02a1e9d361a8c38f874d4e149111800adfdcb
Christian Christiansen <christian.l.christiansen@gmail.com>
2023-10-24 11:33:56 +0200

    Double quotes instead of single quotes

--- reconnect_compliance/calculate_ramp_rates.R
+++ reconnect_compliance/calculate_ramp_rates.R
@@ -5,7 +5,7 @@ calculate_ramp_rates <- function(normalised_power_profiles) {
     mutate(
       ramp_rate = (
         c_id_daily_norm_power - lag(c_id_daily_norm_power)
-      ) / as.numeric(difftime(ts, lag(ts), units = 'mins'))
+      ) / as.numeric(difftime(ts, lag(ts), units = "mins"))
     )
   normalised_power_profiles <- select(normalised_power_profiles, ts, c_id, ramp_rate)
   normalised_power_profiles <- as.data.frame(normalised_power_profiles)
