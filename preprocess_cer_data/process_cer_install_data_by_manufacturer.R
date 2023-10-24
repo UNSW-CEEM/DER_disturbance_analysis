db1e622b49c1cd83a276f16c12ded50934d6c692
Christian Christiansen <christian.l.christiansen@gmail.com>
2023-10-24 11:18:37 +0200

    Change assign operator sign

--- preprocess_cer_data/process_cer_install_data_by_manufacturer.R
+++ preprocess_cer_data/process_cer_install_data_by_manufacturer.R
@@ -4,8 +4,8 @@ library(dplyr)
 intall_data_file <- "cer_data/cer_test_data.csv"
 install_data <- read.csv(file = intall_data_file, header = TRUE, stringsAsFactors = FALSE)
 
-start_date = min(install_data$date)
-end_date = max(install_data$date)
+start_date <- min(install_data$date)
+end_date <- max(install_data$date)
 date_vector <- seq(as.Date(start_date), as.Date(end_date), by = "days")
 
 manufacturers <- unique(install_data$manufacturer)
