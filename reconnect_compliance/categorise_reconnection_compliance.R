8530bb29df6449931aaf83173040ac96e85cd408
Cynthujah <133620267+Cynthujah@users.noreply.github.com>
2023-12-19 16:25:06 +1100

    no_changes_categorize_reconnection_compliance
    There are no changes in "categorize_reconnection_compliance.R"

--- reconnect_compliance/categorise_reconnection_compliance.R
+++ reconnect_compliance/categorise_reconnection_compliance.R
@@ -22,16 +22,15 @@ categorise_row <- function(response_category,
 
 categorise_reconnection_compliance <- function(ramping_data,
                                                ramp_threshold_for_compliance,
-                                               ramp_threshold_for_non_compliance) {
-  ramping_data <- rowwise(ramping_data) %>%
-    mutate(
-      reconnection_compliance_status = categorise_row(
-        response_category,
-        ramp_above_threshold,
-        ramp_threshold_for_compliance,
-        ramp_threshold_for_non_compliance
-      )
-    ) %>%
-    select(c_id, reconnection_compliance_status)
+                                               ramp_threshold_for_non_compliance){
+
+  ramping_data <- rowwise(ramping_data) %>% mutate(
+    reconnection_compliance_status = catergorise_row(response_category,
+                                                     ramp_above_threshold,
+                                                     ramp_threshold_for_compliance,
+                                                     ramp_threshold_for_non_compliance))
+  
+  ramping_data <- select(ramping_data, c_id, reconnection_compliance_status)
+
   return(ramping_data)
 }
