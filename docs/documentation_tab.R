32c6537f6fa254cecfc7cd97a9ddfbaedd90b843
Cynthujah <133620267+Cynthujah@users.noreply.github.com>
2023-12-22 08:53:50 +1100

    Typo "duration" is corrected

--- docs/documentation_tab.R
+++ docs/documentation_tab.R
@@ -331,7 +331,7 @@ documentation_panel <- function() {
       are ordered randomly but with a consistent seed value, so the order will remain consistent."
     ),
     h4("Ideal Reponse Curve"),
-    div("'Ideal Response Curve (Ideal reconnection profile)' illustrates changes over dutation. Potential modifications
+    div("'Ideal Response Curve (Ideal reconnection profile)' illustrates changes over duration. Potential modifications
     to the code is necessary to align the 'Ideal Response Curve' with the 'reconnection power profile'."
         )
   )
