a129ed32b1432436311fed000de39ee15c343ae7
Phoebe <53634732+phoebeheywood@users.noreply.github.com>
2024-01-23 14:39:33 +1100

    change how pu voltages are shown

--- out_of_tool_processing/create_voltage_kml.R
+++ out_of_tool_processing/create_voltage_kml.R
@@ -10,74 +10,108 @@ create_voltage_kml <- function(data) {
       "<?xml version=\"1.0\" encoding=\"UTF-8\"?>",
       "<kml xmlns=\"http://www.opengis.net/kml/2.2\">",
       "  <Document>",
+      "    <name>Manual pu voltages</name>",
       "    <Style id=\"110kV\">",
       "      <LabelStyle>",
       "        <color>ff0000ff</color>",
       "        <width>1.3</width>",
       "      </LabelStyle>",
+      "      <IconStyle>",
+      "        <Icon/>",
+      "      </IconStyle>",
       "    </Style>",
       "    <Style id=\"132kV\">",
       "      <LabelStyle>",
       "        <color>ff0000ff</color>",
       "        <width>1.3</width>",
       "      </LabelStyle>",
+      "      <IconStyle>",
+      "        <Icon/>",
+      "      </IconStyle>",
       "    </Style>",
       "    <Style id=\"275kV\">",
       "      <LabelStyle>",
       "        <color>ffff00aa</color>",
       "        <width>3</width>",
       "      </LabelStyle>",
+      "      <IconStyle>",
+      "        <Icon/>",
+      "      </IconStyle>",
       "    </Style>",
       "    <Style id=\"66kV\">",
       "      <LabelStyle>",
       "        <color>ff1872cc</color>",
       "        <width>1.3</width>",
       "      </LabelStyle>",
+      "      <IconStyle>",
+      "        <Icon/>",
+      "      </IconStyle>",
       "    </Style>",
       "    <Style id=\"33kV\">",
       "      <LabelStyle>",
       "        <color>ff00aa00</color>",
       "        <width>1.3</width>",
       "      </LabelStyle>",
+      "      <IconStyle>",
+      "        <Icon/>",
+      "      </IconStyle>",
       "    </Style>",
       "    <Style id=\"22kV\">",
       "      <LabelStyle>",
       "        <color>ffffaa55</color>",
       "        <width>1.3</width>",
       "      </LabelStyle>",
+      "      <IconStyle>",
+      "        <Icon/>",
+      "      </IconStyle>",
       "    </Style>",
       "    <Style id=\"11kV\">",
       "      <LabelStyle>",
       "        <color>ffffaa55</color>",
       "        <width>1.3</width>",
       "      </LabelStyle>",
+      "      <IconStyle>",
+      "        <Icon/>",
+      "      </IconStyle>",
       "    </Style>",
       "    <Style id=\"220kV\">",
       "      <LabelStyle>",
       "        <color>ffff0000</color>",
       "        <width>2</width>",
       "      </LabelStyle>",
+      "      <IconStyle>",
+      "        <Icon/>",
+      "      </IconStyle>",
       "    </Style>",
       "    <Style id=\"330kV\">",
       "      <LabelStyle>",
       "        <color>ff00aaff</color>",
       "        <width>3</width>",
       "      </LabelStyle>",
+      "      <IconStyle>",
+      "        <Icon/>",
+      "      </IconStyle>",
       "    </Style>",
       "    <Style id=\"400kV\">",
       "      <LabelStyle>",
       "        <color>ffffaa55</color>",
       "        <width>3</width>",
       "      </LabelStyle>",
+      "      <IconStyle>",
+      "        <Icon/>",
+      "      </IconStyle>",
       "    </Style>",
       "    <Style id=\"500kV\">",
       "      <LabelStyle>",
       "        <color>ff00ffff</color>",
       "        <width>4</width>",
       "      </LabelStyle>",
+      "      <IconStyle>",
+      "        <Icon/>",
+      "      </IconStyle>",
       "    </Style>"
     ),
-    collapse="\n"
+    collapse = "\n"
   )
   data <- data %>%
     rowwise %>%
@@ -94,9 +128,9 @@ create_voltage_kml <- function(data) {
     }) %>%
     select(kml_snippet) %>%
     unlist()
-  kml_body <- paste(data, collapse="\n")
+  kml_body <- paste(data, collapse = "\n")
   kml_footer <- "  </Document>\n</kml>"
-  kml_output <- paste(c(kml_header, kml_body, kml_footer), collapse="\n")
+  kml_output <- paste(c(kml_header, kml_body, kml_footer), collapse = "\n")
   return(kml_output)
 }
 
@@ -104,20 +138,20 @@ create_placemark_kml <- function(label, voltage, nominal_line_voltage, latitude,
   placemark_kml <- paste(
     c(
      "    <Placemark>",
-     paste0("      <name>", label, "</name>"),
-     paste0("      <description>Voltage: ", voltage, "kV</description>"),
+     paste0("      <name>", voltage, "</name>"),
+     paste0("      <description>", label, "</description>"),
      paste0("      <styleUrl>#", nominal_line_voltage, "</styleUrl>"),
      "      <Point>",
      paste0("        <coordinates>", longitude, ",", latitude, "</coordinates>"),
      "      </Point>",
      "    </Placemark>"
     ),
-    collapse="\n"
+    collapse = "\n"
   )
   return(placemark_kml)
 }
 
 ## ## Code to uncomment
-## data <- read.csv("voltage.csv", sep=",")
+## data <- read.csv("voltage.csv", sep = ",")
 ## output <- create_voltage_kml(data)
 ## cat(output, file = "output.kml")
